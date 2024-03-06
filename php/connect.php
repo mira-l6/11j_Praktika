212c9c484b53d20d2816a6020bf3e26342352bc1
katerinacookies <sokolyt@gmail.com>
2024-03-06 15:36:58 +0200

    proverka na vruzkata

--- php/connect.php
+++ php/connect.php
@@ -3,13 +3,26 @@
     //$promenliva = $_POST["promenliva"];
 
     //vruzka s bd
-    $conn = new mysqli('localhost', 'root', '', '');
-    if($conn->connect_error)
+    $conn = new mysqli('localhost', 'root', 'test', 'ime na bd');
+
+    //proverka na vruzkata
+    if($conn->connect_error || !$conn)
     {
-        die('Connection Failed : '.$conn->connect_error);
+        die('Connection error: '.mysqli_connect_error());
     }
     else
     {
-        $stmt = $conn->prepare("insert into registration(promenliva")
+        $stmt = $conn->prepare("insert into registration(promenliva) values(?)");
+        //s - string
+        //d - float
+        //i - int
+        //b - blob
+        $stmt->bind_param("s", $promenliva);
+        $stmt->execute();
+        echo "registration succesfully...";
+        $stmt->close();
+        $conn->close();
     }
+
+    
 ?>
