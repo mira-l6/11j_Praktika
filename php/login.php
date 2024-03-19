a9898addedd55fcdbd66f11f62966d3fab6173e2
katerinacookies <sokolyt@gmail.com>
2024-03-19 19:01:52 +0200

    proverka

--- php/login.php
+++ php/login.php
@@ -2,8 +2,31 @@
 
     if(isset($_POST['username']) && isset($_POST['password']))
     {
-        $username = $_POST['username'];
-        $password = $_POST['password'];
+        function validate($data)
+        {
+            $data = trim($data);
+            $data = stripslashes($data);
+            $data = htmlspecialchars($data);
+            return $data;
+        }
+
+        $username = validate($_POST['username']);
+        $password = validate($_POST['password']);
+
+        if(empty($username))
+        {
+            header("Location: login.html?error=Потребителското име е задължително!");
+            exit();
+        }
+        else if(empty($password))
+        {
+            header("Location: login.html?error=Паролата е задължителна!");
+            exit();
+        }
+        else
+        {
+
+        }
     }
     else
     {
