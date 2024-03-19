c425bfff68d7ea58eb3b57ac62cce218d41c70ef
katerinacookies <sokolyt@gmail.com>
2024-03-19 19:47:04 +0200

    login :)

--- php/login.php
+++ php/login.php
@@ -1,5 +1,10 @@
 <?php
 
+    //?
+    session_start();
+
+
+    include "db_connection.php";
     if(isset($_POST['username']) && isset($_POST['password']))
     {
         function validate($data)
@@ -25,7 +30,33 @@
         }
         else
         {
-
+            $sql = "SELECT * FROM `login` WHERE `login_Username`='$username' AND `login_Pass`='$password'"; 
+            $result = mysqli_query($con, $sql);
+            if(mysqli_num_rows($result) === 1)
+            {
+                $row = mysqli_fetch_assoc($result);
+                //print_r($row);
+                if($row['login_Username'] === $username && $row['login_Pass'] === $password)
+                {
+                    $_SESSION['login_ID'] = $row['login_ID'];
+                    $_SESSION['login_Username'] = $row['login_Username'];
+                    $_SESSION['login_Pass'] = $row['login_Pass'];
+                    $_SESSION['login_RealtorID'] = $row['login_RealtorID'];
+                    //?
+                    header("Location: connect.php");
+                    exit();
+                }
+                else
+                {
+                    header("Location: login.html?error=Грешно потребителско име или парола");
+                    exit();
+                }
+            }
+            else
+            {
+                header("Location: login.html?error=Грешно потребителско име или парола");
+                exit();
+            }
         }
     }
     else
