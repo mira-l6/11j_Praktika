fd0b5bbd39ce15876cb52e3809d13ae63700fa67
katerinacookies <sokolyt@gmail.com>
2024-03-20 08:38:09 +0200

    vruzka s bd

---
+++ php/db_connection.php
@@ -0,0 +1,12 @@
+<?php
+ 
+    $host="localhost";
+    $port=3306;
+    $user="root";
+    $password="";
+    $dbname="ForumDB";
+
+    $con = new mysqli($host, $user, $password, $dbname, $port)
+    or die ('Could not connect to the database server' . mysqli_connect_error());
+
+    //$con->close();
