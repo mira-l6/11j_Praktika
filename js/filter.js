6968906908e2cb9952ec2bbc69e67d5da3fb8f5c
PetarRaychev <petarraychev_j20@schoolmath.eu>
2024-04-11 14:34:43 +0300

    Working model for js filter region selector

--- js/filter.js
+++ js/filter.js
@@ -1,34 +1,35 @@
 const townArea = document.getElementById('town-area');
 const town = document.getElementById('populated-place');
 
+
 town.addEventListener('change', AddProperRegion)
 
 function AddProperRegion(){
-    let townOption = townArea.getElementsByTagName('option').length;
-
-    console.log(townOption);
+    // const townOption = townArea.getElementsByTagName('option');
     
-    // switch(townOption){
-    //     case 'град Благоевград':
-    //         town.innerHTML = `
-    //         <option value="">Ален мак</option>
-    //         <option value="">Балабарши</option>
-    //         <option value="">Бялата висота</option>
-    //         <option value="">Варошак</option>
-    //         <option value="">Втора промишлена зона</option>
-    //         <option value="">Грамада</option>`
-    //         break;
+    
 
-    //     case 'град Бургас':
-    //         town.innerHTML = `
-    //         <option value="">Акациите</option>
-    //         <option value="">Банево</option>
-    //         <option value="">Брята Миладинови</option>
-    //         <option value="">Ветрен</option>
-    //         <option value="">Възраждане</option>
-    //         <option value="">Горно езерово</option>`
-    //         break;
-    // }
+    switch(town.value){
+        case 'tblagoevgrad':
+            townArea.innerHTML = `
+            <option value="">Ален мак</option>
+            <option value="">Балабарши</option>
+            <option value="">Бялата висота</option>
+            <option value="">Варошак</option>
+            <option value="">Втора промишлена зона</option>
+            <option value="">Грамада</option>`
+        break;
+
+        case 'град Бургас':
+            townArea.innerHTML = `
+             <option value="">Акациите</option>
+             <option value="">Банево</option>
+             <option value="">Брята Миладинови</option>
+             <option value="">Ветрен</option>
+             <option value="">Възраждане</option>
+             <option value="">Горно езерово</option>`
+        break;
+    }
 
 
 }
