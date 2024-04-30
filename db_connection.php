9a143384460de40e2fa45696f284a29ff89fb828
katerinacookies <sokolyt@gmail.com>
2024-04-30 11:33:08 +0300

    db con change

--- db_connection.php
+++ db_connection.php
@@ -4,7 +4,7 @@
     $port=3306;
     $user="cpses_zhj8iwy9p0@localhost";
     $password="";
-    $dbname="ForumDB";
+    $dbname="zhojxvsm_test";
 
     $con = new mysqli($host, $user, $password, $dbname, $port)
     or die ('Could not connect to the database server' . mysqli_connect_error());
