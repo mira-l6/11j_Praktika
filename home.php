5548f710b5753d8e30f0374ab6132758efa69f37
katerinacookies <sokolyt@gmail.com>
2024-04-30 11:57:22 +0300

    zaqvki home

--- home.php
+++ home.php
@@ -5,7 +5,54 @@
     include "db_connection.php";
 
     //select from vsichki tablici
-    $sql = "SELECT * FROM `` ORDER BY `upload` DESC LIMIT 100"; 
+    $sql = "SELECT * FROM `business_property`, `garage`, `hotel`, `house`, `house_floor`,  
+            `industrial_premise`, `land`, `maisonette`, `office`, `plot`, `realtor`, `restaurant`, `room1`, `room2`, `room3`, `room4`, `roomn`, `shop`, `storage`, `studio`, `villa` ORDER BY `upload` DESC LIMIT 100"; 
+    $sql2 = "SELECT * FROM (
+        SELECT * FROM business_property ORDER BY upload DESC LIMIT 100
+        UNION ALL
+        SELECT * FROM garage ORDER BY upload DESC LIMIT 100
+        UNION ALL
+        SELECT * FROM hotel ORDER BY upload DESC LIMIT 100
+        UNION ALL
+        SELECT * FROM house ORDER BY upload DESC LIMIT 100
+        UNION ALL
+        SELECT * FROM house_floor ORDER BY upload DESC LIMIT 100
+        UNION ALL
+        SELECT * FROM industrial_premise ORDER BY upload DESC LIMIT 100
+        UNION ALL
+        SELECT * FROM land ORDER BY upload DESC LIMIT 100
+        UNION ALL
+        SELECT * FROM maisonette ORDER BY upload DESC LIMIT 100
+        UNION ALL
+        SELECT * FROM office ORDER BY upload DESC LIMIT 100
+        UNION ALL
+        SELECT * FROM plot ORDER BY upload DESC LIMIT 100
+        UNION ALL
+        SELECT * FROM realtor ORDER BY upload DESC LIMIT 100
+        UNION ALL
+        SELECT * FROM restaurant ORDER BY upload DESC LIMIT 100
+        UNION ALL
+        SELECT * FROM room1 ORDER BY upload DESC LIMIT 100
+        UNION ALL
+        SELECT * FROM room2 ORDER BY upload DESC LIMIT 100
+        UNION ALL
+        SELECT * FROM room3 ORDER BY upload DESC LIMIT 100
+        UNION ALL
+        SELECT * FROM room4 ORDER BY upload DESC LIMIT 100
+        UNION ALL
+        SELECT * FROM roomn ORDER BY upload DESC LIMIT 100
+        UNION ALL
+        SELECT * FROM shop ORDER BY upload DESC LIMIT 100
+        UNION ALL
+        SELECT * FROM storage ORDER BY upload DESC LIMIT 100
+        UNION ALL
+        SELECT * FROM studio ORDER BY upload DESC LIMIT 100
+        UNION ALL
+        SELECT * FROM villa ORDER BY upload DESC LIMIT 100
+    ) AS combined_data
+    ORDER BY upload DESC
+    LIMIT 100";
+    
     $result = mysqli_query($con, $sql);
     $row = mysqli_fetch_assoc($result);
     
