54ca32687238aa88ce81c2c1153779f5571ef392
katerinacookies <sokolyt@gmail.com>
2024-05-10 19:28:18 +0300

    w

--- login.php
+++ login.php
@@ -1,14 +1,9 @@
 <?php
-
     //?
     session_start();
 
-
     include "db_connection.php";
-
-    //promenlivi za navigaciqta
-    $header = "https://forumimotibg.com/adminprofile.html";
-    $replace = true;
+    
 
     if(isset($_POST['username']) && isset($_POST['password']))
     {
@@ -35,14 +30,12 @@
         }
         else
         {
-            $sql = $mysqli->execute_query("SELECT * FROM `login` WHERE `login_Username`=? AND `login_Pass`=?", [$username, $password]); 
-            echo "$ sql querito";
+            $sql = $con->query("SELECT * FROM `login` WHERE `login_Username`='$username' AND `login_Pass`='$password'"); 
+            // $sql = $mysqli->execute_query("SELECT * FROM `login` WHERE `login_Username`=? AND `login_Pass`=?", [$username, $password]); 
             $result = mysqli_query($con, $sql);
-            echo "vlizam v ifa";
             if(mysqli_num_rows($result) === 1)
             {
                 $row = mysqli_fetch_assoc($result);
-                echo "vzimam red";
                 //print_r($row);
                 if($row['login_Username'] === $username && $row['login_Pass'] === $password)
                 {
