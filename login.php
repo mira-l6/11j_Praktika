19309a2a49b0f7f29b66c78e72a518895c25ecb8
katerinacookies <sokolyt@gmail.com>
2024-05-10 22:36:47 +0300

    Mitko opravi logina hihi

--- login.php
+++ login.php
@@ -5,73 +5,46 @@
     include "db_connection.php"; 
     
 
-    if(isset($_POST['username']) && isset($_POST['password']))
-    {
-        function validate($data)
-        {
-            $data = trim($data);
-            $data = stripslashes($data);
-            $data = htmlspecialchars($data);
-            return $data;
-        }
+   if(isset($_POST['username']) && isset($_POST['password'])) {
+    $username = trim($_POST['username']);
+    $password = trim($_POST['password']);
 
-        $username = validate($_POST['username']);
-        $password = validate($_POST['password']);
+    if(empty($username)) {
+        header("Location: login.html?error=Потребителското име е задължително!");
+        exit();
+    } elseif(empty($password)) {
+        header("Location: login.html?error=Паролата е задължителна!");
+        exit();
+    } else {
+        $sql = "SELECT * FROM `login` WHERE `login_Username`='$username' AND `login_Pass`='$password'";
+        $result = mysqli_query($con, $sql);
 
-        if(empty($username))
-        {
-            header("Location: login.html?error=Потребителското име е задължително!");
-            exit();
-        }
-        else if(empty($password))
-        {
-            header("Location: login.html?error=Паролата е задължителна!");
-            exit();
-        }
-        else
-        {
-            $sql = $con->query("SELECT * FROM `login` WHERE `login_Username`='$username' AND `login_Pass`='$password'"); 
-            // $sql = $mysqli->execute_query("SELECT * FROM `login` WHERE `login_Username`=? AND `login_Pass`=?", [$username, $password]); 
-            $result = mysqli_query($con, $sql);
-            if(mysqli_num_rows($result) === 1)
-            {
-                $row = mysqli_fetch_assoc($result);
-                //print_r($row);
-                if($row['login_Username'] === $username && $row['login_Pass'] === $password)
-                {
-                    $_SESSION['login_ID'] = $row['login_ID'];
-                    $_SESSION['login_Username'] = $row['login_Username'];
-                    $_SESSION['login_Pass'] = $row['login_Pass'];
-                    $_SESSION['login_RealtorID'] = $row['login_RealtorID'];
-                    
-                    if($row['login_Username'] === "admin")
-                    {
-                        header("Location: adminprofile.html");
-                        exit();
-                    }
-                    else
-                    {
-                        header("Location: profile.html");
-                        exit();
-                    }                   
-                }
-                else
-                {
-                    echo "Ups";
-                    header("Location: login.html?error=Грешно потребителско име или парола");
+        if(mysqli_num_rows($result) === 1) {
+            $row = mysqli_fetch_assoc($result);
+
+            if($row['login_Username'] === $username && $row['login_Pass'] === $password) {
+                $_SESSION['login_ID'] = $row['login_ID'];
+                $_SESSION['login_Username'] = $row['login_Username'];
+                $_SESSION['login_Pass'] = $row['login_Pass'];
+                $_SESSION['login_RealtorID'] = $row['login_RealtorID'];
+
+                if($row['login_Username'] === "admin") {
+                    header("Location: adminprofile.html");
                     exit();
-                }
-            }
-            else
-            {
+                } else {
+                    header("Location: profile.html");
+                    exit();
+                }                   
+            } else {
                 header("Location: login.html?error=Грешно потребителско име или парола");
                 exit();
             }
+        } else {
+            header("Location: login.html?error=Грешно потребителско име или парола");
+            exit();
         }
     }
-    else
-    {
-        header("Location: login.html?не бачкам");
-        exit();
-    }
-    
+} else {
+    header("Location: login.html?не бачкам");
+    exit();
+}
