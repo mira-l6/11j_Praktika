f87c766eb6e333fd368bc741d6094d70ed6692b6
mira-l6 <miralambreva19960@gmail.com>
2024-05-11 20:39:45 +0300

    m

--- js/uploadMultiplePreview.js
+++ js/uploadMultiplePreview.js
@@ -1,49 +1,24 @@
-let images = [];
 
-function imageSelect() {
 
-    let image = document.getElementById('image-upload-multiple').files;
+let fileInput = document.getElementById('image-upload-multiple');
+let imageContainer = document.getElementById('container');
 
-    for (let i = 0; i < image.length; i++) {
-
-        if (checkDuplicate(image[i].name)) {
-            images.push({
-                "name": image[i].name,
-                "url": URL.createObjectURL(image[i]),
-                "file": image[i]
-            })
-        } else {
-            window.alert(image[i].name + "is already displayed!")
+function preview(){
+    imageContainer.innerHTML='';
+    for(i of fileInput.files){
+        let reader = new FileReader();
+        let figure = document.createElement("figure");
+        let figCap = document.createElement('figcaption');
+        figCap.innerText = i.name;
+        figure.appendChild(figCap);
+        reader.onload = ()=>{
+            let img = document.createElement('img');
+            img.setAttribute('src', reader.result);
+            figure.insertBefore(img, figCap);
         }
-
+        figure.classList.add('image-box', 'd-flex', 'flex-column', 'align-items-center');
+        
+        imageContainer.appendChild(figure);
+        reader.readAsDataURL(i);
     }
-    document.getElementById('form').reset();
-    document.getElementById('container').innerHTML = imageShow();
-}
-function imageShow() {
-    var image = "";
-    images.forEach((i) => {
-        image += `<div class="image-box d-flex justify-content-center position-relative">
-                        <img src="` + i.url + `" alt="offer image">
-                        <span class="position-absolute material-icons" onclick="deleteImage(` + images.indexOf(i) + `)">cancel</span>
-                    </div>`;
-    })
-    return image;
-}
-function deleteImage(e) {
-    images.splice(e, 1);
-    document.getElementById('container').innerHTML = imageShow();
-}
-function checkDuplicate(image) {
-    let result = true;
-    if (images.lenght > 0) {
-        for (i = 0; i < images.length; i++) {
-            if (images[i].name == image) {
-                result = false;
-                break;
-            }
-        }
-    }
-    return result;
-
 }
