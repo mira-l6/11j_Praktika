38b5cb016f30b3c48f6ab0914a6785fbe87c6f9d
katerinacookies <sokolyt@gmail.com>
2024-05-25 21:48:57 +0300

    probvam pak

--- filter.php
+++ filter.php
@@ -13,33 +13,75 @@
     if(isset($_POST['property-type']))
     {
         $property_type = trim($_POST['property-type']);
+        $propertytypeqry = "``";
+    }
+    else
+    {
+        $propertytypeqry = "";
     }
     if(isset($_POST['populated-place']))
     {
         $populated_place = trim($_POST['populated-place']);
+        $populatedplaceqry = "";
+    }
+    else
+    {
+        $populatedplaceqry = "";
     }
     if(isset($_POST['town-area']))
     {
         $town_area = trim($_POST['town-area']);
+        $townareaqry = "";
+    }
+    else
+    {
+        $townareaqry = "";
     }
     if(isset($_POST['max-price']))
     {
         $max_price = trim($_POST['max-price']);
+        $maxpriceqry = "";
     }
     else
     {
         $max_price = null;
+        $maxpriceqry = "";
     }
-    if(isset($_POST['quadrature']))
+    if(isset($_POST['min-price']))
     {
-        $quadrature = validate($_POST['quadrature']);
+        $min_price = trim($_POST['min-price']);
+        $minpriceqry = "";
     }
     else
     {
-        $quadrature = null;
+        $min_price = null;
+        $minpriceqry = "";
+    }
+    if(isset($_POST['min_quadrature']))
+    {
+        $min_quadrature = validate($_POST['min_quadrature']);
+        $minquadratureqry = "";
+    }
+    else
+    {
+        $min_quadrature = null;
+        $minquadratureqry = "";
+    }
+    if(isset($_POST['max_quadrature']))
+    {
+        $max_quadrature = validate($_POST['max_quadrature']);
+        $maxquadratureqry = "";
+    }
+    else
+    {
+        $max_quadrature = null;
+        $maxquadratureqry = "";
     }
    
-    $sqlgetoffer = "";
+    //zaqvkata da se konkatinira samo ot nenull stoinostite
+
+    //s for cikul se obhojda masiv s vsichki vidove imoti
+    $sqlgetoffer = "SELECT * FROM `` WHERE ";
     $resultgetoffer = mysqli_query($con, $sqlgetoffer);
     $offerscount = mysqli_num_rows($resultgetoffer);
 
