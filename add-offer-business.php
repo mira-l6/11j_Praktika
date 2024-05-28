94a9595ab343b5134a9dedcf448440e061fb2fed
katerinacookies <sokolyt@gmail.com>
2024-05-28 16:58:31 +0300

    greshen format ili da

--- add-offer-business.php
+++ add-offer-business.php
@@ -14,7 +14,7 @@
     $realtorid = $_SESSION['login_RealtorID'];
             
     $sql = "INSERT INTO `business_property`(`business_Price`, `business_RealtorID`, `business_Quadrature`, `business_FloorFlat`, `business_FloorBuilding`, `business_Gas`, `business_Tpp`, `business_ConstructionYear`, `business_ConstructionType`, `business_Description`, `business_Features`, `business_ForPrivatePeople`, `business_Furnished`, `business_Country`, business_Province`, `business_City`, `business_Region`, `business_Type`, `business_PropertyType`)
-            VALUES ($price, $realtorid, $quadrature, $floorflat, $floors, $gas, $tpp, $constructionyear, $constructiontype, $description, $features, $forprivatepeople, $furnished, $country, $populatedplace, $offername, $townarea, $businesstype, $propertytype)"; 
+            VALUES ('$price', '$realtorid', '$quadrature', '$floorflat', '$floors', '$gas', '$tpp', '$constructionyear', '$constructiontype', '$description', '$features', '$forprivatepeople', '$furnished', '$country', '$populatedplace', '$offername', '$townarea', '$businesstype', '$propertytype')"; 
             $result = mysqli_query($con, $sql);
             if($result)
             {
@@ -39,7 +39,7 @@
             $timeofupload = $rowgettime['business_TimeOfUpload'];
 
             $sqladdoffer = "INSERT INTO `offer`(`offer_Table`, `offer_PropertyID`, `offer_TimeOfUpload`, `offer_Prefix`)
-                        VALUES('business_property', '$last_id', '$timeofupload', `business`)";
+                        VALUES('business_property', '$last_id', '$timeofupload', 'business')";
             $resultaddoffer = mysqli_query($con, $sqladdoffer);
             if(!$resultaddoffer)
             {
--- description.php
+++ description.php
@@ -5,6 +5,38 @@
     if(isset($_POST['description']))
     {
         $description = trim($_POST['description']);
+        if($_POST['gas'] == "on")
+        {
+            $_POST['gas'] = 1;
+        }
+        else
+        {
+            $_POST['gas'] = 0;
+        }
+        if($_POST['tpp'] == "on")
+        {
+            $_POST['tpp'] = 1;
+        }
+        else
+        {
+            $_POST['tpp'] = 0;
+        }
+        if($_POST['forprivatepeople'] == "on")
+        {
+            $_POST['forprivatepeople'] = 1;
+        }
+        else
+        {
+            $_POST['forprivatepeople'] = 0;
+        }
+        if($_POST['furnished'] == "on")
+        {
+            $_POST['furnished'] = 1;
+        }
+        else
+        {
+            $_POST['furnished'] = 0;
+        }
         $gas = $_POST['gas'];
         $tpp = $_POST['tpp'];
         $forprivatepeople = $_POST['forprivatepeople'];
