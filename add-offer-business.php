46c530c34ee6417beb2028ae2b047afd0b4ef0d3
katerinacookies <sokolyt@gmail.com>
2024-05-31 09:44:39 +0300

    probs

--- add-offer-business.php
+++ add-offer-business.php
@@ -41,44 +41,44 @@
         $description = trim($_POST['description']);
         if($_POST['gas'] == "on")
         {
-            $_POST['gas'] = 1;
+            $gas = 1;
         }
         else
         {
-            $_POST['gas'] = 0;
+            $gas = 0;
         }
         if($_POST['tpp'] == "on")
         {
-            $_POST['tpp'] = 1;
+            $tpp = 1;
         }
         else
         {
-            $_POST['tpp'] = 0;
+            $tpp = 0;
         }
         if($_POST['forprivatepeople'] == "on")
         {
-            $_POST['forprivatepeople'] = 1;
+            $forprivatepeople = 1;
         }
         else
         {
-            $_POST['forprivatepeople'] = 0;
+            $forprivatepeople = 0;
         }
         if($_POST['furnished'] == "on")
         {
-            $_POST['furnished'] = 1;
+            $furnished = 1;
         }
         else
         {
-            $_POST['furnished'] = 0;
+            $furnished = 0;
         }
-        $gas = $_POST['gas'];
-        $tpp = $_POST['tpp'];
-        $forprivatepeople = $_POST['forprivatepeople'];
-        $furnished = $_POST['furnished'];
 
         if(isset($_POST['features']))
         {
             $features = trim($_POST['features']);
+        }
+        else
+        {
+            $features = null;
         }
 
     }
