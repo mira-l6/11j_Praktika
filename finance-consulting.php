fe6cdfdc36685ea7359e3ef87cfb13259750e9b1
katerinacookies <sokolyt@gmail.com>
2024-07-28 09:10:59 +0300

    css removed

--- finance-consulting.php
+++ finance-consulting.php
@@ -9,7 +9,6 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="/css/index.css">
     <link rel="stylesheet" href="/css/consulting.css">
-    <link rel="stylesheet" href="/css/footer.css">
     <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
     <title>Форум</title>
 </head>
