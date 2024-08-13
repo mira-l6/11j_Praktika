40621d03136ccb70b4943bd2d6b9f7b531ae1c17
katerinacookies <sokolyt@gmail.com>
2024-08-13 12:38:07 +0300

    sesh start

--- add-agent-proba-php.php
+++ add-agent-proba-php.php
@@ -1,10 +1,12 @@
 <?php
+session_start();
 include "db_connection.php";
 
 
 
 // Handling form submission
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
+if ($_SERVER["REQUEST_METHOD"] == "POST") 
+{
     $first_name = $_POST['first_name'];
     $last_name = $_POST['last_name'];
     $email = $_POST['email'];
