4e3d1bd95d6b0ce95f52d49092e35e98419992f8
katerinacookies <sokolyt@gmail.com>
2024-08-14 10:03:58 +0300

    tip pass

--- add-agent-proba.php
+++ add-agent-proba.php
@@ -106,7 +106,7 @@
             </div>
             <div class="form-group">
                 <label for="password">Парола:</label>
-                <input type="text" id="password" name="password" required>
+                <input type="password" id="password" name="password" required>
             </div>
             <div class="form-group">
                 <label for="phone">Телефон:</label>
