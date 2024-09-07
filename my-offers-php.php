7696ab8f88ed021a0e17dae10d4378612442f8e1
katerinacookies <sokolyt@gmail.com>
2024-09-07 17:03:43 +0300

    proba12

--- my-offers-php.php
+++ my-offers-php.php
@@ -4,6 +4,10 @@
     if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['offer-choice']))
     {
         $offerchoice = $_POST['offer-choice'];
+        if($offerchoice === 'business')
+        {
+            header("Location: contacts.php");
+        }
         $_SESSION['nameofproperty'] = "проба";
 
         header("Location: add-offer.php");
