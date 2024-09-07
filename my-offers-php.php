8b1b5fca9c6c22417375988e6dc0a013f5382254
katerinacookies <sokolyt@gmail.com>
2024-09-07 16:33:27 +0300

    EDNO TIERE>>>

--- my-offers-php.php
+++ my-offers-php.php
@@ -1,7 +1,7 @@
 <?
     session_start();
 
-    if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['offer_choice']))
+    if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['offer-choice']))
     {
         $_SESSION['nameofproperty'] = $_POST['offer-choice'];
 
