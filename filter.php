205eba5652de1d35fe2ce3fa55b7b9d02f04530d
katerinacookies <sokolyt@gmail.com>
2025-07-18 13:16:01 +0300

    debugging

--- filter.php
+++ filter.php
@@ -1,6 +1,9 @@
 <?php
     //?
     //tazi str se izpulnqva sled natiskane na buton filtrirai
+    ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
     session_start();
 
 
